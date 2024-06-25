f82fcc41bf908412eb9b7b0f1fd7c54d96197888
Joel Varghese Oommen <141248492+Joel-VO@users.noreply.github.com>
2024-06-25 15:08:09 +0530

    Update Markov_Model.py

--- Markov_Model.py
+++ Markov_Model.py
@@ -2,7 +2,7 @@ import numpy as np
 import string
 from sklearn.model_selection import train_test_split
 
-#edgar allen is 0, robert is 1
+#0 for Edgar Allen Poe, 1 for Robert Frost
 
 #preprocessing the data, converting a line to its raw form and giving unique index value to each word
 def preprocessing(data_set):
@@ -25,7 +25,7 @@ def preprocessing(data_set):
     unknown_word = id # stores the value for unknown data
     return index, author_labels, text_lines, id ,unknown_word
 
-# Converting the text list into respective numbers using the index dict
+# Converting the text list into respective numbers using the index dictionary
 def to_index(text_lines, index):
     text_indexed = []
     for text in text_lines:
@@ -34,21 +34,21 @@ def to_index(text_lines, index):
         text_indexed.append(temp)
     return text_indexed
 
-# Train the model
-def training(text_indexed, author_labels, id):
+# Training the model
+def training(text_indexed, author_labels, id):#initialize the A and pi arrays
     edgar_A = np.zeros((id, id))
     robert_A = np.zeros((id, id))
     edgar_pi = np.zeros(id)
     robert_pi = np.zeros(id)
 
     for text, label in zip(text_indexed, author_labels):# A and pi matrices are trained by adding 1 for every hit
-        first = text[0]
+        first = text[0]#the pi matrix part
         if label == 0:
             edgar_pi[first] += 1
         elif label == 1:
             robert_pi[first] += 1
 
-        for i in range(1,len(text) - 1):
+        for i in range(1,len(text) - 1):#the A matrix part
             term = text[i]
             next_term = text[i + 1]
             if label == 0:
@@ -58,7 +58,7 @@ def training(text_indexed, author_labels, id):
 
     return edgar_pi, edgar_A, robert_pi, robert_A
 
-# Normalization
+# Normalization of data using add one smoothing and then taking the log of the said matrices
 def normalize_log(pi, A):
     smoothed_pi = pi + 1
     smoothed_A = A + 1
@@ -71,13 +71,13 @@ def normalize_log(pi, A):
     return log_pi, log_A
 
 def predict(text, index, unknown_word, edgar_log_pi, edgar_log_A, robert_log_pi, robert_log_A):
-    #for the pi matrices
     unknown_word = len(edgar_log_pi)-1#has to be introduced due to indexing problems
 
+    # takes the first log probability for each of the groups
     edgar_final_prob = edgar_log_pi[index.get(text[0],unknown_word)]
     robert_final_prob = robert_log_pi[index.get(text[0],unknown_word)]
 
-    #for the A matrix
+    # adds together the log values for each of the author for a given term, term+1 value pair
     for i in range(len(text)-1):
         term = index.get(text[i],unknown_word)
         next_term = index.get(text[i+1],unknown_word)
@@ -85,27 +85,30 @@ def predict(text, index, unknown_word, edgar_log_pi, edgar_log_A, robert_log_pi,
         edgar_final_prob += edgar_log_A[term,next_term]
         robert_final_prob += robert_log_A[term,next_term]
 
-    return 1 if edgar_final_prob<robert_final_prob else 0
+    return 0 if edgar_final_prob>robert_final_prob else 1
 
 
 
-def model_preprocessing():#final methods, we have to split training and data sets
-    data_set = ["D:/edgar_poems.txt","D:/robert_frost.txt"]
+def model_preprocessing():#pre-processes the data
+
+
+    data_set = ["text file here","second text file here"]#change the directory location according to where you've
+                                                            # the dataset
 
     index, author_labels, text_lines, id, unknown_word = preprocessing(data_set)
     text_indexed = to_index(text_lines, index)
     return text_indexed, index, author_labels, text_lines, id, unknown_word
 
-def model_training(text_indexed, author_labels, id):
+def model_training(text_indexed, author_labels, id):#trains the model data
     edgar_pi, edgar_A, robert_pi, robert_A = training(text_indexed, author_labels, id)
     edgar_log_pi, edgar_log_A = normalize_log(edgar_pi, edgar_A)
     robert_log_pi, robert_log_A = normalize_log(robert_pi, robert_A)
     return edgar_log_pi, edgar_log_A, robert_log_pi, robert_log_A
 
 
-def main():
+def main():#main function putting it all together
     text_indexed, index, author_labels, text_lines, id, unknown_word = model_preprocessing()
-    text_indexed_train, text_indexed_test, author_labels_train, author_labels_test = train_test_split(text_indexed, author_labels, test_size=.2, random_state = 42)
+    text_indexed_train, text_indexed_test, author_labels_train, author_labels_test = train_test_split(text_indexed, author_labels, test_size=.1, random_state = 42)
     edgar_log_pi, edgar_log_A, robert_log_pi, robert_log_A = model_training(text_indexed_train, author_labels_train, id)#preprocess trainging and test data
     count = 0
     for text_test, label_test in zip(text_indexed_test, author_labels_test):
