b4ccb4673e8f0fb22ab27713a56f4b5f1dc418a2
Joel Varghese Oommen <141248492+Joel-VO@users.noreply.github.com>
2024-06-26 19:57:55 +0530

    Update Markov_Model.py

--- Markov_Model.py
+++ Markov_Model.py
@@ -1,11 +1,15 @@
 import numpy as np
 import string
 from sklearn.model_selection import train_test_split
-
+from sklearn.metrics import recall_score, f1_score, precision_score
 #0 for Edgar Allen Poe, 1 for Robert Frost
 
-#preprocessing the data, converting a line to its raw form and giving unique index value to each word
+
+"""when trying out the model, make sure the path to the txt files are correct"""
+
+
 def preprocessing(data_set):
+    # preprocessing the data, converting a line to its raw form and giving unique index value to each word
     text_lines = []
     author_labels = []
     index = {}
@@ -25,8 +29,9 @@ def preprocessing(data_set):
     unknown_word = id # stores the value for unknown data
     return index, author_labels, text_lines, id ,unknown_word
 
-# Converting the text list into respective numbers using the index dictionary
+
 def to_index(text_lines, index):
+    # Converting the text list into respective numbers using the index dictionary
     text_indexed = []
     for text in text_lines:
         words = text.split()
@@ -41,7 +46,8 @@ def training(text_indexed, author_labels, id):#initialize the A and pi arrays
     edgar_pi = np.zeros(id+1)
     robert_pi = np.zeros(id+1)
 
-    for text, label in zip(text_indexed, author_labels):# A and pi matrices are trained by adding 1 for every hit
+    for text, label in zip(text_indexed, author_labels):
+        # A and pi matrices are trained by adding 1 for every hit
         first = text[0]#the pi matrix part
         if label == 0:
             edgar_pi[first] += 1
@@ -58,8 +64,9 @@ def training(text_indexed, author_labels, id):#initialize the A and pi arrays
 
     return edgar_pi, edgar_A, robert_pi, robert_A
 
-# Normalization of data using add one smoothing and then taking the log of the said matrices
-def normalize_log(pi, A):
+
+def normalize_log(pi, A):# Normalization of data using add one
+    # smoothing and then taking the log of the said matrices
     smoothed_pi = pi + 1
     smoothed_A = A + 1
 
@@ -71,12 +78,13 @@ def normalize_log(pi, A):
     return log_pi, log_A
 
 
-def priors(author_labels_train):
+def priors(author_labels_train):#the priors are computed to prevent inaccuracies due to assymmetric datasets
     edgar = sum(y==0 for y in author_labels_train)
     robert = sum(y == 1 for y in author_labels_train)
     total = len(author_labels_train)
     edgar_prior = edgar/total
     robert_prior = robert / total
+    #taking log of priors as thats the format of the trained data
     edgar_log_prior = np.log(edgar_prior)
     robert_log_prior = np.log(robert_prior)
     return edgar_log_prior,robert_log_prior
@@ -106,8 +114,8 @@ def predict(text, index, unknown_word, edgar_log_pi, edgar_log_A, robert_log_pi,
 def model_preprocessing():#pre-processes the data
 
 
-    data_set = ["D:/Code/Datasets/edgar_allan_poe.txt","D:/Code/Datasets/robert_frost.txt"]#change the directory location according to where you've
-                                                            # the dataset
+    data_set = ["edgar_allen_poe.txt","robert_frost.txt"]#change the directory location according to where you've
+                                                        # the dataset
 
     index, author_labels, text_lines, id, unknown_word = preprocessing(data_set)
     text_indexed = to_index(text_lines, index)
@@ -121,22 +129,58 @@ def model_training(text_indexed, author_labels, id):#trains the model data
 
 
 def main():#main function putting it all together
-    avg_accuracy = 0
+    #calculating model performance using f1 scoring
+
+    avg_precision = 0
+    avg_f1_score = 0
+    avg_recall = 0
     for i in range(100):
         text_indexed, index, author_labels, text_lines, id, unknown_word = model_preprocessing()
-        text_indexed_train, text_indexed_test, author_labels_train, author_labels_test = train_test_split(text_indexed, author_labels, test_size=.2, random_state = i)
-        edgar_log_pi, edgar_log_A, robert_log_pi, robert_log_A = model_training(text_indexed_train, author_labels_train, id)#preprocess trainging and test data
-        count = 0
-        for text_test, label_test in zip(text_indexed_test, author_labels_test):
-            predicted_label = predict(text_test, index, unknown_word, edgar_log_pi, edgar_log_A, robert_log_pi,
-                                      robert_log_A, author_labels_train)
+        #splitting training and test data
+        text_indexed_train, text_indexed_test, author_labels_train, author_labels_test = train_test_split(text_indexed,
+                                                                                                          author_labels,
+                                                                                                          test_size=.2,
+                                                                                                          random_state = i)
+        edgar_log_pi, edgar_log_A, robert_log_pi, robert_log_A = model_training(text_indexed_train,
+                                                                                author_labels_train,
+                                                                                id)#preprocess training and test data
 
-            if predicted_label == label_test:
-                count+=1
-        accuracy = count/(len(author_labels_test))
-        avg_accuracy += accuracy
-    avg_accuracy /= 100
-    print(f'The model accuracy comes to {avg_accuracy*100:.2f}%')
+        predictions = []#stores the predictions made by the predict method
+        for text_test in (text_indexed_test):
+            predicted_label = predict(text_test, index, unknown_word, edgar_log_pi,
+                                      edgar_log_A, robert_log_pi,
+                                      robert_log_A, author_labels_train)
+            predictions.append(predicted_label)
+        #calculating recall, f1 and precision scores
+        recall = recall_score(author_labels_test,predictions)
+        f1 = f1_score(author_labels_test,predictions)
+        precision = precision_score(author_labels_test,predictions)
+
+        avg_recall += recall
+        avg_f1_score += f1
+        avg_precision += precision
+
+    avg_recall /= 100
+    avg_f1_score /= 100
+    avg_precision /= 100
+
+
+    print(f'The average recall is {avg_recall * 100:.2f}%')
+    print(f'The average f1 score is {avg_f1_score * 100:.2f}%')
+    print(f'The average precision is {avg_precision * 100:.2f}%')
 
 if __name__ == '__main__':
     main()
+
+"""The average recall is 100.00%
+The average f1 score is 79.80%
+The average precision is 66.42%"""
+
+"""The recall value of 100% indicates the model is very capable at capturing true positives
+however the lower value of precision indicates 33.58% of all positives are falsely matched
+
+The model clearly needs improvement specifically in terms of reducing false positives, which can potentially be 
+rectified by improving the training data as the false positives might be arising due to ambiguous text where both 
+authors are equally likely
+
+"""
